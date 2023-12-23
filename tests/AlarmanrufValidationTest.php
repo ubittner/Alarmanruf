fc7559b464dc4b24c5b1735e75ab1edbc7716996
Ulrich Bittner <ubittner@users.noreply.github.com>
2023-12-23 22:06:00 +0100

    added submodules for testing

---
+++ tests/AlarmanrufValidationTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests;
+
+use TestCaseSymconValidation;
+
+include_once __DIR__ . '/stubs/Validator.php';
+
+class AlarmanrufValidationTest extends TestCaseSymconValidation
+{
+    public function testValidateLibrary(): void
+    {
+        $this->validateLibrary(__DIR__ . '/..');
+    }
+
+    public function testValidateModule_Alarmanruf(): void
+    {
+        $this->validateModule(__DIR__ . '/../Alarmanruf');
+    }
+
+    public function testValidateModule_NeXXtMobile(): void
+    {
+        $this->validateModule(__DIR__ . '/../NeXXt Mobile');
+    }
+
+    public function testValidateModule_VoIP(): void
+    {
+        $this->validateModule(__DIR__ . '/../VoIP');
+    }
+}
